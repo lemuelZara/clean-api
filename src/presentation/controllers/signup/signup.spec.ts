09a49aab11de3cccffd2fe67358959efb699c534
Lemuel Zara <lemuel.czara@gmail.com>
2021-03-07 18:32:35 -0300

    test: ensure SignUpController returns 500 if AddAccount throws

--- src/presentation/controllers/signup/signup.spec.ts
+++ src/presentation/controllers/signup/signup.spec.ts
@@ -226,4 +226,25 @@ describe('SignUp Controller', () => {
       password: 'any_password'
     });
   });
+
+  test('Should return 500 if AddAccount throws', () => {
+    const { sut, stubAddAccount } = makeSut();
+
+    jest.spyOn(stubAddAccount, 'add').mockImplementationOnce(() => {
+      throw new Error();
+    });
+
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    };
+    const httpResponse = sut.handle(httpRequest);
+
+    expect(httpResponse.statusCode).toBe(500);
+    expect(httpResponse.body).toEqual(new InternalServerError());
+  });
 });
