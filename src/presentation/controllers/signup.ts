77b050bb45f39ed1c6ae6a33807026a518d94bce
Lemuel Zara <lemuel.czara@gmail.com>
2021-03-06 21:40:44 -0300

    feat: ensure SignUpController returns 400 if no email is provided

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,8 +1,22 @@
 export class SignUpController {
   public handle(httpRequest: any): any {
+    if (!httpRequest.body.name) {
+      return {
+        statusCode: 400,
+        body: new Error('Missing param: name')
+      };
+    }
+
+    if (!httpRequest.body.email) {
+      return {
+        statusCode: 400,
+        body: new Error('Missing param: email')
+      };
+    }
+
     return {
-      statusCode: 400,
-      body: new Error('Missing param: name')
+      statusCode: 200,
+      body: {}
     };
   }
 }
