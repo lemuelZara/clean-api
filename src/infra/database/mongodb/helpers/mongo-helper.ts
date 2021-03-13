adefea4e311d2c66a32f9314ff6d54327e011ad9
Lemuel Zara <lemuel.czara@gmail.com>
2021-03-13 09:12:56 -0300

    feat: ensure AccountMongoRepository returns an account on success

--- src/infra/database/mongodb/helpers/mongo-helper.ts
+++ src/infra/database/mongodb/helpers/mongo-helper.ts
@@ -1,15 +1,18 @@
-import { MongoClient } from 'mongodb';
+import { MongoClient, Collection } from 'mongodb';
 
 let client: MongoClient;
 
 export const MongoHelper = {
   async connect(): Promise<void> {
-    client = await MongoClient.connect(process.env.MONGO_URL, {
+    client = await MongoClient.connect(process.env.MONGO_URL as string, {
       useNewUrlParser: true,
       useUnifiedTopology: true
     });
   },
   async disconnect(): Promise<void> {
     await client.close();
+  },
+  getCollection(name: string): Collection {
+    return client.db().collection(name);
   }
 };
