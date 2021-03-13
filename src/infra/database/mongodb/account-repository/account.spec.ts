d07857ab9b0d129eb0f7a4a82c559f74a696d2ee
Lemuel Zara <lemuel.czara@gmail.com>
2021-03-13 09:13:29 -0300

    test: ensure AccountMongoRepository returns an account on success

---
+++ src/infra/database/mongodb/account-repository/account.spec.ts
@@ -0,0 +1,29 @@
+import { AccountMongoRepository } from './account';
+
+import { MongoHelper } from '../helpers/mongo-helper';
+
+describe('Account MongoDB Repository', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect();
+  });
+
+  afterAll(async () => {
+    await MongoHelper.disconnect();
+  });
+
+  test('Should return an account on success', async () => {
+    const sut = new AccountMongoRepository();
+
+    const account = await sut.add({
+      name: 'any_name',
+      email: 'any_email@mail.com',
+      password: 'any_password'
+    });
+
+    expect(account).toBeTruthy();
+    expect(account.id).toBeTruthy();
+    expect(account.name).toBe('any_name');
+    expect(account.email).toBe('any_email@mail.com');
+    expect(account.password).toBe('any_password');
+  });
+});
