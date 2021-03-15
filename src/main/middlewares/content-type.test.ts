8439c94c6082d98cdd227799e2f8013092f26545
Lemuel Zara <lemuel.czara@gmail.com>
2021-03-14 23:51:23 -0300

    feat: ensure requests return json as default content type

---
+++ src/main/middlewares/content-type.test.ts
@@ -0,0 +1,24 @@
+import request from 'supertest';
+
+import app from '../config/app';
+
+describe('Content Type Middleware', () => {
+  test('Should return default content type as json', async () => {
+    app.get('/test-content-type', (req, res) => {
+      res.send('');
+    });
+
+    await request(app).get('/test-content-type').expect('content-type', /json/);
+  });
+
+  test('Should return xml content type when forced', async () => {
+    app.get('/test-content-type-xml', (req, res) => {
+      res.type('xml');
+      res.send('');
+    });
+
+    await request(app)
+      .get('/test-content-type-xml')
+      .expect('content-type', /xml/);
+  });
+});
