80e1d927bfb9dc39d394b00d2589e29a65140ef6
Lemuel Zara <lemuel.czara@gmail.com>
2021-04-12 21:22:47 -0300

    feat: ensure LoginController returns 500 if EmailValidator throws

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -1,5 +1,5 @@
 import { InvalidParamError, MissingParamError } from '../../errors';
-import { badRequest, ok } from '../../helpers/http-helper';
+import { badRequest, internalServerError, ok } from '../../helpers/http-helper';
 import { Controller, HttpRequest, HttpResponse } from '../../protocols';
 import { EmailValidator } from '../signup/signup-protocols';
 
@@ -11,28 +11,32 @@ export class LoginController implements Controller {
   }
 
   public async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    const { email, password } = httpRequest.body;
+    try {
+      const { email, password } = httpRequest.body;
 
-    if (!email) {
-      return new Promise((resolve) =>
-        resolve(badRequest(new MissingParamError('email')))
-      );
+      if (!email) {
+        return new Promise((resolve) =>
+          resolve(badRequest(new MissingParamError('email')))
+        );
+      }
+
+      if (!password) {
+        return new Promise((resolve) =>
+          resolve(badRequest(new MissingParamError('password')))
+        );
+      }
+
+      const isValid = this.emailValidator.isValid(email);
+
+      if (!isValid) {
+        return new Promise((resolve) =>
+          resolve(badRequest(new InvalidParamError('email')))
+        );
+      }
+
+      return new Promise((resolve) => resolve(ok({})));
+    } catch (error) {
+      return internalServerError(error);
     }
-
-    if (!password) {
-      return new Promise((resolve) =>
-        resolve(badRequest(new MissingParamError('password')))
-      );
-    }
-
-    const isValid = this.emailValidator.isValid(email);
-
-    if (!isValid) {
-      return new Promise((resolve) =>
-        resolve(badRequest(new InvalidParamError('email')))
-      );
-    }
-
-    return new Promise((resolve) => resolve(ok({})));
   }
 }
