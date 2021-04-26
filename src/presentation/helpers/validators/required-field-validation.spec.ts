9863d6710d08c2305fd34245d9a8efd553110ea5
Lemuel Zara <lemuel.czara@gmail.com>
2021-04-26 20:54:41 -0300

    refactor: update import path

--- src/presentation/helpers/validators/required-field-validation.spec.ts
+++ src/presentation/helpers/validators/required-field-validation.spec.ts
@@ -1,5 +1,5 @@
 import { MissingParamError } from '../../errors';
-import { RequiredFieldValidation } from './required-field-validation-';
+import { RequiredFieldValidation } from './required-field-validation';
 
 const makeSut = (): RequiredFieldValidation => {
   return new RequiredFieldValidation('field');
