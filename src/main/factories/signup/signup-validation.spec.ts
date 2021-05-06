64dcc5fcd8d820cb7434ae367b34f4d60723257d
Lemuel Zara <lemuel.czara@gmail.com>
2021-05-05 21:30:59 -0300

    refactor: group validation imports in a single file

--- src/main/factories/signup/signup-validation.spec.ts
+++ src/main/factories/signup/signup-validation.spec.ts
@@ -1,10 +1,12 @@
 import { makeSignUpValidation } from './signup-validation';
 
-import { ValidationComposite } from '../../../presentation/helpers/validators/validation-composite';
-import { RequiredFieldValidation } from '../../../presentation/helpers/validators/required-field-validation';
+import {
+  CompareFieldsValidation,
+  EmailValidation,
+  RequiredFieldValidation,
+  ValidationComposite
+} from '../../../presentation/helpers/validators';
 import { Validation } from '../../../presentation/protocols/validation';
-import { CompareFieldsValidation } from '../../../presentation/helpers/validators/compare-fields-validation';
-import { EmailValidation } from '../../../presentation/helpers/validators/email-validation';
 import { EmailValidator } from '../../../presentation/protocols/email-validator';
 
 jest.mock('../../../presentation/helpers/validators/validation-composite');
--- src/main/factories/signup/signup-validation.ts
+++ src/main/factories/signup/signup-validation.ts
@@ -1,8 +1,10 @@
-import { CompareFieldsValidation } from '../../../presentation/helpers/validators/compare-fields-validation';
-import { EmailValidation } from '../../../presentation/helpers/validators/email-validation';
-import { RequiredFieldValidation } from '../../../presentation/helpers/validators/required-field-validation';
+import {
+  CompareFieldsValidation,
+  EmailValidation,
+  RequiredFieldValidation,
+  ValidationComposite
+} from '../../../presentation/helpers/validators';
 import { Validation } from '../../../presentation/protocols/validation';
-import { ValidationComposite } from '../../../presentation/helpers/validators/validation-composite';
 import { EmailValidatorAdapter } from '../../../presentation/utils/email-validator-adapter';
 
 export const makeSignUpValidation = (): ValidationComposite => {
