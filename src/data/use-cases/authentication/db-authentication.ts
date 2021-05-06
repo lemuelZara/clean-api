dd4648babaa27a0b7231d77cc1e502e04123d129
Lemuel Zara <lemuel.czara@gmail.com>
2021-05-05 23:04:09 -0300

    feat: ensure DbAuthentication calls LoadAccountByEmailRepository correct email

---
+++ src/data/use-cases/authentication/db-authentication.ts
@@ -0,0 +1,19 @@
+import {
+  Authentication,
+  AuthenticationModel
+} from '../../../domain/use-cases/authentication';
+import { LoadAccountByEmailRepository } from '../../protocols/load-account-by-email-repository';
+
+export class DbAuthentication implements Authentication {
+  private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository;
+
+  constructor(loadAccountByEmailRepository: LoadAccountByEmailRepository) {
+    this.loadAccountByEmailRepository = loadAccountByEmailRepository;
+  }
+
+  async auth(authentication: AuthenticationModel): Promise<string> {
+    await this.loadAccountByEmailRepository.load(authentication.email);
+
+    return null;
+  }
+}
