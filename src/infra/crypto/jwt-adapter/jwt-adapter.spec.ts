4b2cc85bebd201f5360abb661ec95e3a07a61156
Lemuel Zara <lemuel.czara@gmail.com>
2021-06-23 14:11:42 -0300

    test: ensure JWTAdapter throws if sign throws

--- src/infra/crypto/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/crypto/jwt-adapter/jwt-adapter.spec.ts
@@ -26,4 +26,16 @@ describe('JWT Adapter', () => {
 
     expect(accessToken).toBe('any_token');
   });
+
+  test('Should throw if sign throws', async () => {
+    const sut = new JWTAdapter('secret');
+
+    jest.spyOn(jwt, 'sign').mockImplementationOnce(() => {
+      throw new Error();
+    });
+
+    const promise = sut.encrypt('any_id');
+
+    await expect(promise).rejects.toThrow();
+  });
 });
