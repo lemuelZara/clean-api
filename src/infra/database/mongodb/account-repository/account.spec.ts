4484316b1e1cddcc04f0a84bf0b3733c007a7837
Lemuel Zara <lemuel.czara@gmail.com>
2021-06-26 00:38:35 -0300

    test: ensure AccountMongoRepository updatethe accessToken on updateAccessToken success

--- src/infra/database/mongodb/account-repository/account.spec.ts
+++ src/infra/database/mongodb/account-repository/account.spec.ts
@@ -66,4 +66,25 @@ describe('Account MongoDB Repository', () => {
 
     expect(account).toBeFalsy();
   });
+
+  test('Should update the account accessToken on updateAccessToken success', async () => {
+    const sut = makeSut();
+
+    const {
+      ops: [fakeAccount]
+    } = await accountCollection.insertOne({
+      name: 'any_name',
+      email: 'any_email@mail.com',
+      password: 'any_password'
+    });
+
+    expect(fakeAccount.accessToken).toBeFalsy();
+
+    await sut.updateAccessToken(fakeAccount._id, 'any_token');
+
+    const account = await accountCollection.findOne({ _id: fakeAccount._id });
+
+    expect(account).toBeTruthy();
+    expect(account.accessToken).toBe('any_token');
+  });
 });
