fe3330271890995d26958d79b6a75076152cecc4
Lemuel Zara <lemuel.czara@gmail.com>
2021-06-26 00:26:14 -0300

    feat: ensure AccountMongoRepository returns an account on loadByEmail success

--- src/infra/database/mongodb/account-repository/account.ts
+++ src/infra/database/mongodb/account-repository/account.ts
@@ -1,11 +1,13 @@
 import { AddAccountRepository } from '../../../../data/protocols/database/add-account-repository';
+import { LoadAccountByEmailRepository } from '../../../../data/protocols/database/load-account-by-email-repository';
 
 import { AccountModel } from '../../../../domain/models/account';
 import { AddAccountModel } from '../../../../domain/use-cases/add-account';
 
 import { MongoHelper } from '../helpers/mongo-helper';
 
-export class AccountMongoRepository implements AddAccountRepository {
+export class AccountMongoRepository
+  implements AddAccountRepository, LoadAccountByEmailRepository {
   async add(accountData: AddAccountModel): Promise<AccountModel> {
     const accountCollection = await MongoHelper.getCollection('accounts');
 
@@ -13,5 +15,13 @@ export class AccountMongoRepository implements AddAccountRepository {
     const account = result.ops[0];
 
     return MongoHelper.mapping(account);
+  }
+
+  async loadByEmail(email: string): Promise<AccountModel> {
+    const accountCollection = await MongoHelper.getCollection('accounts');
+
+    const account = await accountCollection.findOne({ email });
+
+    return account && MongoHelper.mapping(account);
   }
 }
